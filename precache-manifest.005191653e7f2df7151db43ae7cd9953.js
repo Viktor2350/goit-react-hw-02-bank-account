0e66d9743fc0256eab3904f8279c659247f364e1
Viktor2350 <ryjuk39@gmail.com>
2019-09-29 12:35:17 +0300

    Updates

---
+++ precache-manifest.005191653e7f2df7151db43ae7cd9953.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f22af36acf73ad825e34b024336a0c8",
+    "url": "/goit-react-hw-02-bank-account/index.html"
+  },
+  {
+    "revision": "4520fbc362c1befa57ac",
+    "url": "/goit-react-hw-02-bank-account/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "0e8baa3f7943535e8513",
+    "url": "/goit-react-hw-02-bank-account/static/css/main.9d323d68.chunk.css"
+  },
+  {
+    "revision": "4520fbc362c1befa57ac",
+    "url": "/goit-react-hw-02-bank-account/static/js/2.76d7298c.chunk.js"
+  },
+  {
+    "revision": "0e8baa3f7943535e8513",
+    "url": "/goit-react-hw-02-bank-account/static/js/main.36660009.chunk.js"
+  },
+  {
+    "revision": "f51494d4f2201f4273ce",
+    "url": "/goit-react-hw-02-bank-account/static/js/runtime-main.51b62e5c.js"
+  }
+]);
